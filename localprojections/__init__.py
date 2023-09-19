03bb2578a0e21f5a3f7ac84069597621c380b8e1
suahjl <62934877+suahjl@users.noreply.github.com>
2023-09-19 19:14:58 +0900

    v0.0.6

---
+++ localprojections/__init__.py
@@ -0,0 +1,18 @@
+# __init__.py
+
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the localprojections package
+__version__ = "0.0.2"
+ 
+# Load scripts / classes / functions so that they can be called directly
+from .lp import (
+    PanelLP,
+    PanelLPX,
+    TimeSeriesLP,
+    IRFPlot,
+)
